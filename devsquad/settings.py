42150d329eed87d4e3173e12b5b13b0f2800cff8
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-07-22 17:55:00 +0530

    deployment

--- devsquad/settings.py
+++ devsquad/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +137,7 @@ STATICFILES_DIRS = [
 
 # to handle user uploaded static files
 MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
