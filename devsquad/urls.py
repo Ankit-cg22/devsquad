83a3103110a793b652d490ec2805174cfb558c18
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-07-22 18:05:59 +0530

    deployment

--- devsquad/urls.py
+++ devsquad/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include , re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
-    re_path(r'^media/(?P<path>.*)$' , serve , {'document_root' : settings.MEDIA_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$' , serve , {'document_root' : settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
